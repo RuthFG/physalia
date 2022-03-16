4b52aa8fe343559572e3db1a2554e124c8636669
Ruth Fernandez <ruth.fernandez@ices.dk>
2022-03-16 16:26:55 +0100

    trying out this

---
+++ basic commads.R
@@ -0,0 +1,9 @@
+#to be able to push I had to create a token
+#to cretae a token that is sent to by git
+usethis::create_github_token()
+
+gitcreds::gitcreds_set()
+#the system asks me for a token
+#? Enter password or token: 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
+
+#all good!
